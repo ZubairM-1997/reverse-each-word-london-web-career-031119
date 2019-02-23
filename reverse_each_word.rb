2263eae1c4ea67506ebbe04dee16d5a9bba58061
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-23 03:07:02 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+
+#.each method
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
+end
+
+
+#.collect method
+
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end
+
